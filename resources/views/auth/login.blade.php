550a02c79e6d828206d10da8ff73c744cd4e8197
Hector Riffo <hriffo@gs1chile.org>
2017-05-04 10:13:27 -0300

    Cambio en el login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,7 +8,7 @@
 <body class="hold-transition login-page">
     <div class="login-box">
         <div class="login-logo">
-            <a href="{{ url('/home') }}"><b>Juako</b>System</a>
+            <a href="{{ url('/home') }}"><b>Juakosss</b>System</a>
         </div><!-- /.login-logo -->
 
     @if (count($errors) > 0)
